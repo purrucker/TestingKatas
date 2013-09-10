99fec67ca92d2f2f814051fa0258075e8af09c31
Daniel <daniel.purrucker@nordakademie.de>
2013-09-10 11:52:46 +0200

    StringCalculatorTests

--- src/main/Acme/StringCalculator.php
+++ src/main/Acme/StringCalculator.php
@@ -4,8 +4,55 @@ namespace Acme;
 
 class StringCalculator
 {
-    public function add($string)
+    public function add($string="")
     {
-        throw new \RuntimeException("@TODO: Implement");
+        $delimiter = $this->findDelimiter($string);
+        $string = $this->findNumbers($string);
+
+        $string = str_replace("\n",$delimiter,$string);
+        $numbers = explode($delimiter,$string);
+        $sum = 0;
+        $negatives = array();
+        foreach($numbers as $number) {
+            if((int) $number < 0) {
+                $negatives[] = (int) $number;
+            } else {
+                $sum += (int) $number;
+            }
+        }
+        if(!empty($negatives)) {
+            $string = "";
+            for($i = 0; $i < sizeof($negatives); $i++) {
+                if($i == sizeof($negatives)-1) {
+                    $string .= $negatives[$i];
+                } else {
+                    $string .= $negatives[$i].",";
+                }
+            }
+            throw new \InvalidArgumentException("No negative numbers allowed ($string)!");
+        } else {
+            return $sum;
+        }
+    }
+
+    /**
+     * @param $string
+     * @return array
+     */
+    private function findDelimiter($string)
+    {
+        $delimiter = ",";
+        if (strpos($string, "//") !== false) {
+            $delimiter = substr($string, 2, strpos($string, "\n") - 2);
+        }
+        return $delimiter;
+    }
+
+    private function findNumbers($string)
+    {
+        if (strpos($string, "//") !== false) {
+            $string = substr($string,strpos($string, "\n"));
+        }
+        return $string;
     }
 }
--- test/Acme/StringCalculatorTest.php
+++ test/Acme/StringCalculatorTest.php
@@ -4,13 +4,123 @@ namespace Acme;
 
 class StringCalculatorTest extends \PHPUnit_Framework_TestCase
 {
-    public function testEmptyString()
-    {
-        $calculator = new StringCalculator();
+    /**
+     * @var StringCalculator
+     */
+    private $calculator;
 
-        $this->assertEquals(
-            0,
-            $calculator->add("")
-        );
+    public function setUp()
+    {
+            $this->calculator = new StringCalculator();
+    }
+
+    /**
+     * @test
+     */
+    public function calculator_gets_empty_string()
+    {
+        $this->assertEquals(0,$this->calculator->add(""));
+    }
+
+    /**
+     * @test
+     */
+    public function calculator_gets_a_single_string() {
+        $this->assertEquals(1,$this->calculator->add('1'));
+    }
+
+    /**
+     * @test
+     */
+    public function calculator_gets_two_strings()
+    {
+        $this->assertEquals(3,$this->calculator->add('1,2'));
+    }
+
+    /**
+     * @test
+     */
+    public function calculator_gets_unknown_amount_of_numbers_in_string()
+    {
+        $amount = rand(1,100);
+        $string = "";
+        $sum = 0;
+        for($i = 1; $i <= $amount; $i++) {
+            $random = rand(1,100);
+            $sum += $random;
+            if($i == $amount) {
+                $string .= $random;
+            } else {
+                $string .= $random.",";
+            }
+        }
+        $this->assertEquals($sum,$this->calculator->add($string));
+    }
+
+    /**
+     * @test
+     */
+    public function calculator_gets_line_break_in_string()
+    {
+        $this->assertEquals(3,$this->calculator->add("1\n2"));
+    }
+
+    /**
+     * @test
+     */
+    public function calculator_gets_line_break_and_comma_in_string()
+    {
+        $this->assertEquals(6,$this->calculator->add("1,2\n3"));
+    }
+
+    /**
+     * @test
+     */
+    public function calculator_gets_defined_delimeter_in_string()
+    {
+        $this->assertEquals(3,$this->calculator->add("//;\n1;2"));
+    }
+
+    /**
+     * @test
+     */
+    public function calculator_gets_long_defined_delimiter_in_string()
+    {
+        $this->assertEquals(6,$this->calculator->add("//___\n1___2___3"));
+    }
+
+    /**
+     * @test
+     */
+    public function calculator_gets_defined_delemiter_and_line_breaks_in_string()
+    {
+        $this->assertEquals(6,$this->calculator->add("//;\n1;2\n3"));
+    }
+
+    /**
+     * @test
+     */
+    public function calculator_gets_negative_number()
+    {
+        $this->setExpectedException("InvalidArgumentException","No negative numbers allowed (-2)!");
+        $this->assertEquals(4,$this->calculator->add("1,-2,3"));
+    }
+
+    /**
+     * @test
+     */
+    public function calculator_gets_more_negative_numbers()
+    {
+        $this->setExpectedException("InvalidArgumentException","No negative numbers allowed (-2,-3,-4)!");
+        $this->assertEquals(1,$this->calculator->add("1,-2,-3,-4"));
+    }
+
+    /**
+     * @test
+     */
+    public function calculator_gets_defined_delimiter_and_negative_numbers()
+    {
+        $this->setExpectedException("InvalidArgumentException","No negative numbers allowed (-2,-3,-4)!");
+        $this->assertEquals(1,$this->calculator->add("//;\n1;-2;-3;-4"));
     }
 }
